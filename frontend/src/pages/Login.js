7e32241f218dccedb58ed47ee21a935d6b44915d
Jamal Kheir <jamalkheir@example.com>
2026-01-04 14:28:32 +0200

    end Updating

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -21,7 +21,7 @@ const Login = () => {
     try {
       setLoading(true);
 
-      const response = await fetch("http://localhost:5000/api/login", {
+      const response = await fetch("https://student-api-r6f7.onrender.com/api/login", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
