4f45e98805bf425c48274b6aaad5c6b276679645
Jamal Kheir <jamalkheir@example.com>
2026-01-04 13:45:56 +0200

    Fix database connection  and dotenv

--- backend/db.js
+++ backend/db.js
@@ -1,15 +1,27 @@
 const mysql = require("mysql2");
 
-const db = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "student"
+const db = mysql.createPool({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT,
+    ssl: {
+        rejectUnauthorized: true
+    },
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0
 });
 
-db.connect(err => {
-  if (err) throw err;
-  console.log("MySQL Connected");
+
+db.query("SELECT 1", (err, results) => {
+  if (err) {
+    console.error("DB Connection Error:", err);
+  } else {
+    console.log("DB Connected Successfully!");
+  }
 });
+
 
 module.exports = db;
